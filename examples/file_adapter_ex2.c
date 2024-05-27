61bfd7516597b672f64b2f8700b896ff468de5ee
Ger Hobbelt <ger@hobbelt.com>
2024-05-27 18:15:17 +0200

    while other stuff (BFCR) still crashes badly, this one at least demonstrates some willingness to work. Tweaked the test so we can run the test data through it more thoroughly. (Curious thing though: feeding it unknown keys still produces (incorrect) hits, so this is still not as clear as one might hope.

--- examples/file_adapter_ex2.c
+++ examples/file_adapter_ex2.c
@@ -13,13 +13,21 @@
 
 int main(int argc, const char **argv)
 {   
+	if (argc < 2)
+	{ 
+		fprintf(stderr, "file_adapter_ex2 <file:keys.txt> <key:jjjjjjjjjj>...\n");
+		return EXIT_FAILURE;
+	}
 	// Open file with newline separated list of keys
-	FILE * keys_fd = fopen("keys.txt", "r");
+	const char* path = "keys.txt";
+	if (argc >= 2)
+		path = argv[1];
+	FILE * keys_fd = fopen(path, "r");
 	cmph_t *hash = NULL;
 	if (keys_fd == NULL) 
 	{
-	  fprintf(stderr, "File \"keys.txt\" not found\n");
-	  exit(1);
+	  fprintf(stderr, "File \"%s\" not found\n", path);
+		return EXIT_FAILURE;
 	}	
 	// Source of keys
 	cmph_io_adapter_t *source = cmph_io_nlfile_adapter(keys_fd);
@@ -28,14 +36,21 @@ int main(int argc, const char **argv)
 	cmph_config_set_algo(config, CMPH_BDZ);
 	hash = cmph_new(config);
 	cmph_config_destroy(config);
-   
-	// Find key
-	const char *key = "jjjjjjjjjj";
-	unsigned int id = cmph_search(hash, key, (cmph_uint32)strlen(key));
-	fprintf(stderr, "Id:%u\n", id);
+
+	do {
+		// Find key
+		const char* key = "jjjjjjjjjj";
+		if (argc >= 3)
+			key = argv[2];
+		unsigned int id = cmph_search(hash, key, (cmph_uint32)strlen(key));
+		fprintf(stderr, "Key: %s --> Id:%u\n", key, id);
+		argc--;
+		argv++;
+	} while (argc >= 3);
+
 	// Destroy hash
 	cmph_destroy(hash);
 	cmph_io_nlfile_adapter_destroy(source);   
 	fclose(keys_fd);
-	return 0;
+	return EXIT_SUCCESS;
 }
